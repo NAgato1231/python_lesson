c969c287a2f87458cae72a192bbbdf4b401e232c
NAgato1231 <nicerp1@mail.ru>
2021-10-11 20:07:34 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+name = "Hello iman"
+
+def gtint ():
+    name = "Hello iman"
+    a = 2
+    b = 3
+    digit = 18
+    list_of_digits = [2, 3, 5, 20]
+    print(list_of_digits)
+
+
+if __name__ == '__main__':
+     print (name)
+     a = 2
+     b = 3
+     print("This is your homework")
+     digit = 18
+     list_of_digits = [2+1]
+
+     print(list_of_digits)
+
+     list_of_digits = [3+2]
+
+     print (list_of_digits)
+
+     print(int("2")<int("2"))
+     print(int("2")>int("1"))
+     print(int("3")>int("3"))
+     print(int("3")>int("2"))
+     print(type(list_of_digits))
